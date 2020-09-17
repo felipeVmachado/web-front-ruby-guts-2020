3a69062cdc8d79abb8feb470d99b6fc46ca3867f
machadof <machadof@dbserver.com.br>
2020-09-17 15:43:05 -0300

    novo cenario de dados faker

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -2,19 +2,25 @@
     @app.cadastropage.load
   end
 
+
   Dado('solicito um novo cadastro para o email {string}') do |email|
     @app.cadastropage.iniciar_criacao_conta email
   end
-  
+
   Dado('preencho os campos do formulario com dados validos fixos') do
     @app.cadastropage.preencher_form_com_dados_fixos
   end
+
 
   Quando('confirmo o cadastro') do
     @app.cadastropage.confirmar_cadastro
   end
   
+  Dado('preencho os campos do formulario com dados validos aleatorios') do
+    @app.cadastropage.preencher_form_com_dados_aleatorios\
+  end
+
   Entao('devo ser direcionado a pagina da minha conta') do
-    expect(@app.minhacontapage.page_title.text).to eq("MY ACCOUNT")
-    expect(@app.minhacontapage.account_name.text).to eq("Felipe Machado") 
+    expect(@app.minhacontapage.page_title.text).to eq('MY ACCOUNT')
+    expect(@app.minhacontapage.account_name.text).to eq(@app.cadastropage.account_full_name) 
   end
